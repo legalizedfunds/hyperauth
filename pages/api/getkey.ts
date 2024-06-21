7a36e76bd13608456acd8245e1e3ece12133085f
legal <137090888+legalizedfunds@users.noreply.github.com>
2024-06-21 14:14:25 -0500

    Update getkey.ts

--- pages/api/getkey.ts
+++ pages/api/getkey.ts
@@ -14,7 +14,7 @@ function generateTimestampHash(): string {
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const referer = req.headers.referer
-  console.log(req.headers);
+  console.log(document.referer);
   const now = Date.now();
   const cookies = req.cookies;
 
