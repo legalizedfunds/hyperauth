fc50d4123fdd8cddf052ceb2b4ddccb8ef48a1bd
legal <137090888+legalizedfunds@users.noreply.github.com>
2024-06-26 12:06:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,16 +6,6 @@ const PORT = 3000;
 const BLACKLIST = ['bypass.city', 'bypass', 'undefined'];
 const ALLOWLIST = ['linkvertise.com'];
 
-app.use((req, res, next) => {
-    const ref = req.get('referer');
-
-    if (ref && ref[BLACKLIST]) {
-        res.send("phuck u");
-    } else (ref && !ref[BLACKLIST] && ref && ref[ALLOWLIST]); {
-        res.send("welcome");
-    }
-})
-
 app.get('/', (req, res) => {
     res.send('henlo')
 })
